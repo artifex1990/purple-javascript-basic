27e69cf70d351eb8ad8e3d999a2118bfe01d9a58
pavel demyanov <pavel@demjanov.ru>
2025-02-22 20:22:18 +0300

    Арифметические операторы

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
-let a = 1;
-console.log(a);
-a = 'test'
-console.log(a);
+
+const width = 10;
+const height = 5;
+const space = width * height;
+const newWidth = width - 4;
+const newWidth2 = width + 4;
+const division = newWidth / newWidth2;
+const volume = 2 ** 3;
+console.log(volume);
+
+
+const city = 'Moscow';
+const street = 'Lenina';
+console.log(city + ', ' + street + ' ' + 5);
