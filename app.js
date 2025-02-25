a63e098da5e60795224b0c94ab83be80c5593fc2
pavel demyanov <pavel@demjanov.ru>
2025-02-25 22:52:02 +0300

    Упражнение - Цикл в обратном порядке

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
-const tasks = ['Задача 1', 'Задача 2', 'Задача 3'];
+const arr = ['!', 'JS', 'люблю', 'Я'];
+const res = [];
 
-for (let i = 0; i < tasks.length; i++) {
-    if (tasks[i] === 'Задача 2') {
-        continue;
-    }
-    console.log(tasks[i]);
+
+for (let i = arr.length - 1; 0 <= i; i--) {
+    res.push(arr[i]);
 }
-console.log('----------');
-for (let i = 0; i < tasks.length; i++) {
-    if (tasks[i] === 'Задача 2') {
-        break;
-    }
-    console.log(tasks[i]);
-}
+
+console.log(res.join(' '));
