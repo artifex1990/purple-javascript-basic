4675b7fcb16babfe1a0baad98b3e1b5dfbd3d42c
pavel demyanov <pavel@demjanov.ru>
2025-02-25 15:39:18 +0300

    Параметры по умолчанию

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
-function toPower(num, power) {
+function toPower(num, power = 2) {
     const res = num ** power;
     return res;
 }
 
-console.log(toPower(6, 2));
-
-const toRefactorPower = (num, power) => num ** power;
-console.log(toRefactorPower(5, 3));
+console.log(toPower(6, 3));
+console.log(toPower(4));
