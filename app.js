fd5552b841cbc3deca9c6aba574b866802de226b
pavel demyanov <pavel@demjanov.ru>
2025-02-25 12:43:38 +0300

    Упражнение - Проверка робота

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const bmwX3Price = 10_0000;
-const fordFocusPrice = 10_000;
-const budget = 20_000;
-
-let message = budget > bmwX3Price 
-? 'BMW' 
-: 'Велосипед';
-
-console.log(`Я хочу купить ${message}`);
+const res = prompt('Сколько будет 7 + или - 15?');
+switch (true) {
+    case res == 'Я не робот':
+    case Number(res) === 22:
+    case Number(res) === -8:
+        console.log('Успех');
+        break;
+    default:
+        console.log('Вы робот!');
+}
