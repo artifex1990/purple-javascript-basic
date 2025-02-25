aaf646dafe39537c84fda705e5b58b0001b13168
pavel demyanov <pavel@demjanov.ru>
2025-02-25 13:58:19 +0300

    Упражнение - Проверка прав

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
-let age = 0;
-console.log(age || 18);
-console.log(age ?? 18);
+const balance = 1200;
+const bonusBalance = 90;
+const isBanned = false;
+const isExist = false;
+const isSelling = true;
+
+const canBy = (balance > 1000 || bonusBalance > 100) 
+    && !isBanned
+    && !isExist
+    && isSelling;
+
+console.log(`Могу купить игру: ${canBy ? 'Да' : 'Нет'}`);
