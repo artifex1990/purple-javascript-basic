92b207de52450f256cac7a90db84752caccbcacf
pavel demyanov <pavel@demjanov.ru>
2025-02-25 13:46:29 +0300

    Операторы с другими типами

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
+console.log('Вася' || 'Олег');
+console.log(false || 'Олег');
+console.log('Вася' || false);
+console.log(false || false);
+
+console.log('Вася' && 'Олег');
+console.log(false && 'Олег');
+console.log('Вася' && false);
+console.log(false && false);
+
+
+let a = 'Марина';
+const username = a || 'Петя';
+console.log(username);
+
 const isAdmin = true;
-const canWrite = true;
-
-console.log(`Системный файл ${isAdmin && canWrite}`);
-console.log(`Обычный файл ${isAdmin || canWrite}`);
-console.log(`Инвертируем права админа ${!isAdmin}`);
-
-const isEdited = true;
-const isSuperAdmin = true;
-
-console.log(`Системный файл с редактированием ${
-    isAdmin && canWrite && (!isEdited || isSuperAdmin)
-}`);
+const filename = isAdmin && 'file.mp4';
+console.log(filename);
