e3f88e8b891c153b7c8329b2177764c5f39436df
pavel demyanov <pavel@demjanov.ru>
2025-02-25 15:35:22 +0300

    Упражнение - Упрощение функции

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-function powerOfTwo(num) {
-    return num * num;
+function toPower(num, power) {
+    const res = num ** power;
+    return res;
 }
 
-console.log(powerOfTwo(5));
+console.log(toPower(6, 2));
 
-const poft = num => num * num; 
-
-console.log(poft(6));
+const toRefactorPower = (num, power) => num ** power;
+console.log(toRefactorPower(5, 3));
