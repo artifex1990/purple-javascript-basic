ea152cb3584311a3864fb88863aaea90bae55732
pavel demyanov <pavel@demjanov.ru>
2025-02-25 15:50:47 +0300

    Функции в функциях

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
-function canAccessWebsite(age) {
-    if (age < 18) {
-        return 'Нет';
-    }
-        
-    return 'Да';
+const KG_IN_USD = 7;
+const KM_IN_USD = 5;
+
+function calculateW(present) {
+    return present * KG_IN_USD;
 }
 
-console.log(canAccessWebsite(16));
+function calculateW(distance) {
+    return distance * KM_IN_USD;
+}
 
-const canAccessWebsite2 = age => age < 18 ? 'Нет' : 'Да';
-console.log(canAccessWebsite2(19));
+function getExchangePrice(present1, present2, distance) {
+    const price1 = calculateW(present1);
+    const price2 = calculateW(present2);
+    const distancePrice = calculateW(distance);
+
+    return price1 + price2 + distancePrice;
+}
+
+console.log(getExchangePrice(1, 2, 10));
