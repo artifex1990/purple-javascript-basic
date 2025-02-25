f882c5bc0b3838f830ae8514fd1b4872dc1606b8
pavel demyanov <pavel@demjanov.ru>
2025-02-25 15:43:14 +0300

    Условия в функциях

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
-function toPower(num, power = 2) {
-    const res = num ** power;
-    return res;
+function canAccessWebsite(age) {
+    if (age < 18) {
+        return 'Нет';
+    }
+        
+    return 'Да';
 }
 
-console.log(toPower(6, 3));
-console.log(toPower(4));
+console.log(canAccessWebsite(16));
+
+const canAccessWebsite2 = age => age < 18 ? 'Нет' : 'Да';
+console.log(canAccessWebsite2(19));
