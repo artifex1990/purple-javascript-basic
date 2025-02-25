acae3cc7ae38307ebb1b25c282520feab8d05cb3
pavel demyanov <pavel@demjanov.ru>
2025-02-25 22:38:49 +0300

    Break и continue

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
-for (let i = 1; i < 10; i++) {
-    console.log(`Ваш баланс ${i}$`);
+const tasks = ['Задача 1', 'Задача 2', 'Задача 3'];
+
+for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i] === 'Задача 2') {
+        continue;
+    }
+    console.log(tasks[i]);
+}
+console.log('----------');
+for (let i = 0; i < tasks.length; i++) {
+    if (tasks[i] === 'Задача 2') {
+        break;
+    }
+    console.log(tasks[i]);
 }
