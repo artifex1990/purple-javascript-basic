c72f3696217348c31119e0c162465531bbbfc9bd
pavel demyanov <pavel@demjanov.ru>
2025-02-25 03:39:11 +0300

    Тернарные операторы

--- app.js
+++ app.js
@@ -1,24 +1,9 @@
-const role = 'fuck';
+const bmwX3Price = 10_0000;
+const fordFocusPrice = 10_000;
+const budget = 20_000;
 
+let message = budget > bmwX3Price 
+? 'BMW' 
+: 'Велосипед';
 
-switch (role) {
-    case 'manager': console.log('manager'); break;
-    case 'admin': console.log('admin'); break;
-    case 'ceo': console.log('ceo'); break;
-    default: console.log('I can`t you');
-}
-
-switch (role) {
-    case 'manager':
-    case 'admin': console.log('Isn`t boss'); break;
-    case 'ceo': console.log('ceo'); break;
-    default: console.log('I can`t you');
-}
-
-const num = 0;
-
-switch (true) {
-    case num > 0: console.log('positive'); break;
-    case num < 0: console.log('negative'); break;
-    default: console.log('zero'); break;
-}
+console.log(`Я хочу купить ${message}`);
