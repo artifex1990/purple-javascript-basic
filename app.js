793ad37aa24099eee4e347b754ce660841af2e45
pavel demyanov <pavel@demjanov.ru>
2025-02-26 00:42:20 +0300

    Упражнение - Расчёт итогового баланса

--- app.js
+++ app.js
@@ -1,13 +1,55 @@
-const arr = [1, 4, 8, 7];
+const operations = [1_000, -700, 300, -500, 10_000];
+const startingBalance = 100;
 
-for (let i = 0; i < arr.length; i++) {
-    console.log(arr[i]);
+function getBalance(operations, initialBalance) {
+    let balance = initialBalance;
+    for (const el of operations) {
+        balance += el;
+    }
+
+    return balance;
 }
 
-for (let element of arr) {
-    console.log(element);
+function checkOperations(operations, initialBalance) {
+    let balance = initialBalance;
+    let isOk = true;
+    for (const el of operations) {
+        balance += el;
+
+        if (balance < 0) {
+            isOk = false;
+            break;
+        }
+    }
+
+    return isOk;
 }
 
-for (let index in arr) {
-    console.log(arr[index]);
+function avarageOperations(operations, startingBalance) {
+    const fixedNum = 2;
+    let positiveSum = 0;
+    let negativeSum = 0;
+    let positiveCount = 0;
+    let negativeCount = 0;
+
+    for (let el of operations) {
+        if( el > 0) {
+            positiveSum += el;
+            positiveCount++;
+        } 
+        if (el < 0) {
+            negativeSum += el;
+            negativeCount++;
+        }
+    }
+
+    positiveSum /= positiveCount;
+    negativeSum /= negativeCount;
+
+    return [+positiveSum.toFixed(fixedNum), +negativeSum.toFixed(fixedNum)];
 }
+
+
+console.log(getBalance(operations, startingBalance));
+console.log(checkOperations(operations, startingBalance));
+console.log(avarageOperations(operations));
