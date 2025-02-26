d52d66d551b8ac6d0fcebb26483b6e01446f76ee
pavel demyanov <pavel@demjanov.ru>
2025-02-26 21:35:14 +0300

    Быстрое создание массивов

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
-const users = ['Вася', 'Петя', 'Маша'];
-console.log(users);
-users.sort();
-console.log(users);
+const arr = [1, 2, 3, 4, 5];
 
-const operations = [100, -300, -100, 50, 480];
-console.log(operations);
-operations.sort((a, b) => a - b);
-console.log(operations);
+console.log(arr);
+console.log(new Array(1, 2, 3, 4, 5));
 
-operations.sort((a, b) => b - a);
-console.log(operations);
+const arr2 = new Array(5);
+console.log(arr2);
+arr2.fill(1, 0, 3);
+arr2.fill(2, 3, 5);
+console.log(arr2);
+
+const arr3 = Array.from({length: 5}, (cur, i) => i + 1);
+console.log(arr3);
