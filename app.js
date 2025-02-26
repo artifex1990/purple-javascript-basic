6d70aea485d2f687b91227fab7bd3c52f1e2352b
pavel demyanov <pavel@demjanov.ru>
2025-02-26 15:03:30 +0300

    map

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const score = [5, 10, 15, 20, 25, 30, 35, 0, 45, 50, 55, 10, 65, 20, 75, 80, 85, 90, 95, 100];
+const transactionInUSD = [10, -7, 50, -10, 100];
 
-// for (const [i, el] of score.entries()) {
-//     console.log(`Раунд ${i + 1}: ${el}`);
-// }
+const transactionInRUB = transactionInUSD
+    .map((transaction) => transaction * 60);
 
-score.forEach((el, i) => console.log(`Раунд ${i + 1}: ${el}`));
+console.log(transactionInUSD);
+console.log(transactionInRUB);
