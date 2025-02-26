23ad025182bbf0f5874f611fe86479185e917fc0
pavel demyanov <pavel@demjanov.ru>
2025-02-26 13:28:46 +0300

    Упражнение - Стрелочные функции

--- app.js
+++ app.js
@@ -1,9 +1,4 @@
-function power(pow) {
-    return function (num) {
-        return num ** pow;
-    }
-}
-
+const power = pow => num => num ** pow;
 const powerOfTwo = power(2);
 console.log(powerOfTwo(5));
 console.log(power(10)(2));
