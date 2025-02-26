25940f83c006ad9b310c256c59719625a2508c2c
pavel demyanov <pavel@demjanov.ru>
2025-02-26 20:41:53 +0300

    reduce

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
-const prices = [[100, 200], [120, 100], [200, 350]];
+const operations = [100, -20, 7, -30, 50];
 
-const result = prices
-    .map(product => product[1] - product[0]).
-    filter(price => price > 0);
-    
-console.log(result);
+let balance = 0;
+for (const operation of operations) {
+    balance += operation;
+}
+
+console.log(balance);
+
+const finalBalance = operations.reduce((acc, operation) => acc + operation, 0);
+console.log(finalBalance);
+
+
+const minOperation = operations.reduce((acc, operation) => acc > operation ? operation : acc);
+console.log(minOperation);
