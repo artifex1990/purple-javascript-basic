676773e8c45e3f5909c603d961e48a278d50bfa0
pavel demyanov <pavel@demjanov.ru>
2025-02-26 23:04:23 +0300

    Преобразование строки

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-const userName = 'Вася aka Terminator Пупкин';
-const [name, , , lastName] = userName.split(' ');
-const fullUserName = 
-    userName.slice(0, userName.indexOf(' ')) 
-    + 
-    userName.slice(userName.lastIndexOf(' '), userName.length);
+const str = 'Вася Пупкин';
+console.log(str.includes('а'));
+console.log(str.startsWith('Вася'));
+console.log(str.endsWith('Пупкин'));
+console.log(new String('Вася Пупкин').includes('а'));
 
-console.log(fullUserName);
+console.log(str.toLowerCase());
+console.log(str.toUpperCase());
+console.log(str.replace('Пупкин', 'Вася'));
+console.log(str.replaceAll('а', 'и'));
+console.log(str.replace(/а/g, 'и'));
+
+const str2 = ' Вася Пупкин  \n';
+console.log(str2.trim());
+console.log(str2.trimStart());
+console.log(str2.trimEnd());
