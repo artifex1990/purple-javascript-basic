adfcd9f8580911cc3688af8b12ceee9d4a8c9b66
pavel demyanov <pavel@demjanov.ru>
2025-02-26 21:02:38 +0300

    find и findIndex

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const arr = [2, 4, 4, 10];
 
-const avg = arr
-    .reduce((acc, el, i) => i !== arr.length - 1 ? acc + el : (acc + el) / arr.length, 0);
-console.log(avg);
+const elGT5 = arr.find(el => el > 5);
+console.log(elGT5);
+
+const elGT5Index = arr.findIndex(el => el < 0);
+console.log(elGT5Index);
