197eb5999547e0ec32e9fe095c3116931ec71436
pavel demyanov <pavel@demjanov.ru>
2025-02-26 13:16:18 +0300

    Callback

--- app.js
+++ app.js
@@ -1,55 +1,12 @@
-const operations = [1_000, -700, 300, -500, 10_000];
-const startingBalance = 100;
+function sum(a, b) { return a + b; }
+function subtract(a, b) { return a - b; }
+function power(a, b) { return a ** b; }
+function calculate(a, b, fn) { 
+    console.log(fn.name);
+    const res = fn(a, b);
+    return res;
+ }
 
-function getBalance(operations, initialBalance) {
-    let balance = initialBalance;
-    for (const el of operations) {
-        balance += el;
-    }
-
-    return balance;
-}
-
-function checkOperations(operations, initialBalance) {
-    let balance = initialBalance;
-    let isOk = true;
-    for (const el of operations) {
-        balance += el;
-
-        if (balance < 0) {
-            isOk = false;
-            break;
-        }
-    }
-
-    return isOk;
-}
-
-function avarageOperations(operations, startingBalance) {
-    const fixedNum = 2;
-    let positiveSum = 0;
-    let negativeSum = 0;
-    let positiveCount = 0;
-    let negativeCount = 0;
-
-    for (let el of operations) {
-        if( el > 0) {
-            positiveSum += el;
-            positiveCount++;
-        } 
-        if (el < 0) {
-            negativeSum += el;
-            negativeCount++;
-        }
-    }
-
-    positiveSum /= positiveCount;
-    negativeSum /= negativeCount;
-
-    return [+positiveSum.toFixed(fixedNum), +negativeSum.toFixed(fixedNum)];
-}
-
-
-console.log(getBalance(operations, startingBalance));
-console.log(checkOperations(operations, startingBalance));
-console.log(avarageOperations(operations));
+ console.log(calculate(1, 2, sum));
+ console.log(calculate(1, 2, subtract));
+ console.log(calculate(123, 2, power));
