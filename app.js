9bd83672b66c290504783b153209963e7ed5d962
pavel demyanov <pavel@demjanov.ru>
2025-02-27 22:53:14 +0300

    Строки и массивы

--- app.js
+++ app.js
@@ -1,24 +1,8 @@
-function isPhoneNumber(num) {
-    let phone = num.trim();
-    phone = phone.replace('+7', '8');
-
-    if (!phone.startsWith('8')) {
-        return false;
-    }
-
-    phone = phone.replaceAll('(', '');
-    phone = phone.replaceAll(')', '');
-    phone = phone.replaceAll(' ', '');
-    phone = phone.replaceAll('-', '');
-
-    if (phone.length !== 11) {
-        return false;
-    }
-
-    return !isNaN(phone);
-}
-
-
-console.log(isPhoneNumber('8(999)999-99-99'));
-console.log(isPhoneNumber('+7(999)999-99-99'));
-console.log(isPhoneNumber('+7999999h999'));
+const userFullName = 'Вася Пупкин Васильевич';
+console.log(userFullName.split(''));
+const [firstName, familyName, lastName] = userFullName.split(' ');
+console.log(firstName);
+console.log(familyName);
+console.log(lastName);
+const arr = ['Ты', 'знаешь', 'JS'];
+console.log(arr.join(' '));
