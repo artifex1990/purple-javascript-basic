d0e13cf0dca515172ccdddc17f5336159bcf74d2
pavel demyanov <pavel@demjanov.ru>
2025-02-28 14:25:19 +0300

    Упражнение - склад

--- app.js
+++ app.js
@@ -1,17 +1,48 @@
-const cities = {
-    msk: {
-        temp: {
-            celcius: 25
-        }
-    },
-    spb: {
-
+const warehouse = {
+  goods: [],
+  findGoodById: function (id) {
+    return this.goods.find(g => g.id == id);
+  },
+  addGood: function (good) {
+    const existedGood = this.findGoodById(good.id);
+    if (existedGood) {
+        console.log('Товар с таким id уже есть в магазине');
+        return;
     }
+    this.goods.push(good);
+  },
+  getWeightKg: function () {
+    return this.goods.reduce((acc, el) => acc += el.weight?.kg ? el.weight.kg : 0, 0);
+  },
+};
+
+/* Товары */
+const car = {
+  id: 1,
+  weight: {
+  kg: 1000
+  },
+  brand: 'Ford'
 }
 
-const city = 'spb';
-if (cities[city] && cities[city].temp) {
-    console.log(cities[city].temp.celcius)
+const chair = {
+  id: 2,
+  weight: {
+    kg: 2
+  }
 }
 
-console.log(cities[city]?.temp?.celcius);
+const paper = {
+  id: 3,
+  color: 'red'
+}
+
+warehouse.addGood(car);
+warehouse.addGood(car);
+warehouse.addGood(chair);
+warehouse.addGood(paper);
+console.log(warehouse.goods);
+let findedItem = warehouse.findGoodById(1);
+console.log(findedItem);
+const weight = warehouse.getWeightKg();
+console.log(weight);
