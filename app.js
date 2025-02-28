e381ae5c6ec2a48101daf000b1fb8bd1a17586e0
pavel demyanov <pavel@demjanov.ru>
2025-02-28 11:40:10 +0300

    Упражнение - сортировка пользователей

--- app.js
+++ app.js
@@ -1,26 +1,8 @@
-const userArray = ['Вася', 'Пупкин', 24];
+const users = [
+    { name: 'Вася', age: 30 },
+    { name: 'Катя', age: 18 },
+    { name: 'Аня', age: 40 },
+    { name: 'Петя', age: 25 },
+];
 
-const user = {
-    name: 'Вася',
-    surname: 'Пупкин',
-    age: 24,
-    skills: [
-        'Программирование',
-        'Готовка'
-    ],
-    eduBasic: 'School 3',
-    eduPro: 'BGTU'
-};
-
-console.log(user);
-console.log(user.skills);
-console.log(user['skills']);
-const level = 'Pro';
-console.log(user['edu' + level]);
-
-user['city'] = 'Moscow';
-console.log(user.city);
-
-user.age = 30;
-user['age'] = 30;
-console.log(user);
+console.log(users.sort((a, b) => a.age - b.age));
