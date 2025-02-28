f814d26cb4a6078364fcf9466ee4a6efc5235564
pavel demyanov <pavel@demjanov.ru>
2025-02-28 11:56:50 +0300

    Методы объектов

--- app.js
+++ app.js
@@ -1,14 +1,9 @@
-const users = [
-    { name: 'Вася', age: 30 , surname: 'Пупкина', skills: ['coding', 'pentest', 'dating'] },
-    { name: 'Катя', age: 30 , surname: 'Васина', skills: ['hacking'] },
-    { name: 'Аня', age: 30 , surname: 'Шлепина', skills: ['coding', 'pentest'] },
-    { name: 'Петя', age: 30 , surname: 'Виноградов', skills: ['coding', 'pentest', 'dating'] },
-];
+const user = {
+    name: 'Вася', 
+    age: 30 , 
+    surname: 'Пупкин', 
+    skills: ['coding', 'pentest', 'dating'],
+    getFullName: function () { return this.name + ' ' + this.surname },
+};
 
-const userData = users.map(user => { 
-    return { 
-        fullName: user.name + ' ' + user.surname,
-        skillNum: user.skills.length 
-    };
-});
-console.log(userData);
+console.log(user.getFullName());
