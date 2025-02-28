ac04894c1d2ab3d0d2437d78653c492f45272467
pavel demyanov <pavel@demjanov.ru>
2025-02-28 12:17:07 +0300

    Упражнение - кошелёк

--- app.js
+++ app.js
@@ -1,9 +1,29 @@
-const user = {
-    name: 'Вася', 
-    age: 30 , 
-    surname: 'Пупкин', 
-    skills: ['coding', 'pentest', 'dating'],
-    getFullName: function () { return this.name + ' ' + this.surname },
-};
+const wallet = {
+    balance: 0,
+    operations: [],
+    increase: function (sum, reason) {
+        this.balance += sum;
+        this.operations.push({reason: reason, sum: sum});
 
-console.log(user.getFullName());
+        return true;
+    },
+    decrease: function (sum, reason) {
+        if (sum > this.balance) {
+            console.log('Недостаточный баланс!');
+            return false;
+        }
+
+        this.balance -= sum;
+        this.operations.push({reason: reason, sum: -sum});
+
+        return true;
+    },
+    getCountOperations: function () { return this.operations.length },
+}
+
+console.log(wallet.increase(100, 'deposit'));
+console.log(wallet.increase(100, 'salary'));
+console.log(wallet.decrease(13, 'tax'));
+console.log(wallet);
+console.log(wallet.operations);
+console.log(wallet.balance);
