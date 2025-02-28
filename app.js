48cfb30172b80b9b1e9ce1291d8baa8324dbd7d9
pavel demyanov <pavel@demjanov.ru>
2025-02-28 11:34:33 +0300

    Обращение к элементам

--- app.js
+++ app.js
@@ -8,5 +8,19 @@ const user = {
         'Программирование',
         'Готовка'
     ],
-    1: 1
+    eduBasic: 'School 3',
+    eduPro: 'BGTU'
 };
+
+console.log(user);
+console.log(user.skills);
+console.log(user['skills']);
+const level = 'Pro';
+console.log(user['edu' + level]);
+
+user['city'] = 'Moscow';
+console.log(user.city);
+
+user.age = 30;
+user['age'] = 30;
+console.log(user);
