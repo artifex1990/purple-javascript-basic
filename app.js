24cd028554c2877e7c4cdfeea16700eff629a797
pavel demyanov <pavel@demjanov.ru>
2025-03-01 13:03:49 +0300

    Примитивы и объекты

--- app.js
+++ app.js
@@ -1,48 +1,17 @@
-const warehouse = {
-  goods: [],
-  findGoodById: function (id) {
-    return this.goods.find(g => g.id == id);
-  },
-  addGood: function (good) {
-    const existedGood = this.findGoodById(good.id);
-    if (existedGood) {
-        console.log('Товар с таким id уже есть в магазине');
-        return;
-    }
-    this.goods.push(good);
-  },
-  getWeightKg: function () {
-    return this.goods.reduce((acc, el) => acc += el.weight?.kg ? el.weight.kg : 0, 0);
-  },
+'use strict';
+
+let firstName = 'Action';
+let firstName2 = firstName;
+firstName = 'Hero';
+console.log(firstName);
+console.log(firstName2);
+
+const user1 = {
+  name: 'John',
 };
 
-/* Товары */
-const car = {
-  id: 1,
-  weight: {
-  kg: 1000
-  },
-  brand: 'Ford'
-}
+const user2 = user1;
 
-const chair = {
-  id: 2,
-  weight: {
-    kg: 2
-  }
-}
-
-const paper = {
-  id: 3,
-  color: 'red'
-}
-
-warehouse.addGood(car);
-warehouse.addGood(car);
-warehouse.addGood(chair);
-warehouse.addGood(paper);
-console.log(warehouse.goods);
-let findedItem = warehouse.findGoodById(1);
-console.log(findedItem);
-const weight = warehouse.getWeightKg();
-console.log(weight);
+user2.name = 'Tom';
+console.log(user1);
+console.log(user2);
