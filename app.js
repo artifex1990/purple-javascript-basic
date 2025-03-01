1030666ba71de225371ef87d2b5ce5186f043300
pavel demyanov <pavel@demjanov.ru>
2025-03-01 13:12:07 +0300

    Пример примитивов и объектов

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 'use strict';
 
-let firstName = 'Action';
-let firstName2 = firstName;
-firstName = 'Hero';
-console.log(firstName);
-console.log(firstName2);
-
-const user1 = {
+const user = {
   name: 'John',
+  id: 1,
+  roles: ['Admin']
 };
 
-const user2 = user1;
+const newUser = Object.assign({}, user);
+newUser.name = 'NewUser';
 
-user2.name = 'Tom';
-console.log(user1);
-console.log(user2);
+
+const newUser2 = {
+  ...user
+};
+newUser2.name = 'NewUser2';
+newUser2.roles.push('NewRole');
+console.log(user);
+console.log(newUser);
+console.log(newUser2);
