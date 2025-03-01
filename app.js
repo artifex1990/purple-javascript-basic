370f43c30b5b30feed93724181da490cf30ee209
pavel demyanov <pavel@demjanov.ru>
2025-03-01 16:06:48 +0300

    Strict mode

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
-'use strict';
+'use strict'
 
-const user = {
-  name: 'John',
-  id: 1,
-  roles: ['Admin']
-};
+let myCoolVariable = 1;
 
-const newUser = Object.assign({}, user);
-newUser.name = 'NewUser';
+if (true) {
+  myCoolVariable = 3; 
+}
 
 
-const newUser2 = {
-  ...user
-};
-newUser2.name = 'NewUser2';
-newUser2.roles.push('NewRole');
-console.log(user);
-console.log(newUser);
-console.log(newUser2);
+console.log(myCoolVariable);
+const interface = 8;
