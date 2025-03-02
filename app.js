8831ab55d06c95b0ebc3b7e69cd045a9dd38b7f7
pavel demyanov <pavel@demjanov.ru>
2025-03-03 00:31:00 +0300

    Local storage

--- app.js
+++ app.js
@@ -44,3 +44,12 @@ newElement.classList.add('panel');
 newElement.innerHTML = `<button class="${panelClass}">${panelText}</button>`;
 
 document.querySelector('.test').appendChild(newElement);
+
+localStorage.setItem('token', 'fadfasf');
+localStorage.setItem('token1', 1);
+localStorage.setItem('token2', true);
+
+const token1 = localStorage.getItem('token1');
+console.log(typeof token1);
+localStorage.removeItem('token2');
+localStorage.clear();
