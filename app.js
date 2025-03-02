3e8c9730847df698de61765c3f1387f0ed76c69b
pavel demyanov <pavel@demjanov.ru>
2025-03-02 12:58:20 +0300

    Обработка событий клавиатуры

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
 //     document.querySelector('.input').value = '';
 // });
 
-function changeClick() {
+function submitForm() {
     const input = document.querySelector('.input').value;
 
     if (!input) {
@@ -20,4 +20,10 @@ function changeClick() {
 
     document.querySelector('.panel').innerText = input;
     document.querySelector('.input').value = '';
+}
+
+function inputChanged(e) {
+    if (e.code === 'Enter') {
+        submitForm();
+    }
 }
