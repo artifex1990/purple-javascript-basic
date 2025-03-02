b336182998973fba31c98b4aad80767154dbb3d4
pavel demyanov <pavel@demjanov.ru>
2025-03-02 09:57:11 +0300

    EOL для методов

--- app.js
+++ app.js
@@ -1,27 +1,15 @@
 'use strict';
 
-const company = {
-    name: 'Company',
-    employees: [
-        {
-            name: 'Ivan',
-            getName: function () {
-                return this.name;
-            }
-        }
-    ],
-    ceo: {
-        name: 'Vasya',
-        getName: function () {
-            return this.name;
-        }
+const b = 1;
+
+const a = {
+    b,
+    getB: function () {
+        return this.b;
     },
-    getNameCompany: function () {
-        return this.name;
-    },
+    getBAlt() {
+        return this.b;
+    }
 }
 
-
-console.log(company.getNameCompany());
-console.log(company.ceo.getName());
-console.log(company.employees.map(employee => employee.getName()));
+console.log(a.getBAlt());
