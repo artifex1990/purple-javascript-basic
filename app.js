ddf07f11e5cd1eee15958d9c1f0d4f62abfc9630
pavel demyanov <pavel@demjanov.ru>
2025-03-03 00:35:31 +0300

    JSON

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
 'use strict';
 
-// document.querySelector('.button').addEventListener('click', function () {
-//     const input = document.querySelector('.input').value;
-
-//     if (!input) {
-//         return;
-//     }
-
-//     document.querySelector('.panel').innerText = input;
-//     document.querySelector('.input').value = '';
-// });
-
 function inputChanged(e) {
     if (e.code === 'Enter') {
         submitForm();
@@ -26,30 +15,10 @@ function submitForm() {
 
     document.querySelector('.panel').innerText = input;
     document.querySelector('.input').value = '';
-    //document.querySelector('.notification').classList.add('notification_active');
-    //document.querySelector('.notification').classList.remove('notification_active');
-    console.log(document.querySelector('.notification').getAttribute('class'));
-    document.querySelector('.notification').setAttribute('class', 'notification');
-    document.querySelector('.notification').setAttribute('key', 1);
-    document.querySelector('.notification').setAttribute('user-id', 1);
-    console.log(document.querySelector('.notification').getAttribute('user-id'));
+    document.querySelector('.notification').classList.remove('notification_hidden');
 }
 
-const panelText = 'Панель';
-const panelClass = 'button';
-const newElement = document.createElement('button');
-newElement.setAttribute('user-id', 1);
-newElement.classList.add('panel');
-// newElement.innerText = 'Кнопка';
-newElement.innerHTML = `<button class="${panelClass}">${panelText}</button>`;
-
-document.querySelector('.test').appendChild(newElement);
-
-localStorage.setItem('token', 'fadfasf');
-localStorage.setItem('token1', 1);
-localStorage.setItem('token2', true);
-
-const token1 = localStorage.getItem('token1');
-console.log(typeof token1);
-localStorage.removeItem('token2');
-localStorage.clear();
+const obj = JSON.parse('{"a": 1}');
+console.log(obj.a);
+const str = JSON.stringify(obj);
+console.log(str);
