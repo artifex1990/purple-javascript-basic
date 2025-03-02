dd155a4914696ef2f963c0a1d13acf4c6c2431df
pavel demyanov <pavel@demjanov.ru>
2025-03-02 10:18:31 +0300

    bind

--- app.js
+++ app.js
@@ -2,33 +2,22 @@
 
 const audi = {
     make: 'Audi',
-    model: 'A4',
-    year: 2020,
-    damages: [],
-    addDamage(part, rate) {
-        console.log(
-            `У авто ${this.make} ${this.model} ${this.year} добавлено повреждение ${part} со степенью ${rate}.`
-        );
-        this.damages.push({part, rate});
-    }
-}
-//audi.addDamage('капот', 1);
-
-const bmw = {
-    make: 'BMW',
-    model: 'X5',
-    year: 2022,
-    damages: [],
+    model: 'A3',
+    damages: []
 };
 
-bmw.addDamage = audi.addDamage;
-//bmw.addDamage('Бампер', 2);
+const carManipulation = {
+    addDamage(part, rate) {
+        this.damages.push({ part, rate });
+        console.log(`Добавить повреждение на ${this.make} ${this.model}.`);
+    }
+}
+
+const addDamageAudi = carManipulation.addDamage.bind(audi);
+addDamageAudi('Кузов', 3);
+console.log(audi);
 
 
-const addDamageFunc = audi.addDamage;
-//addDamageFunc('Бампер', 2);
-addDamageFunc.call(bmw, ...['Бампер', 2]);
-addDamageFunc.call(audi, 'Бампер', 2);
-
-addDamageFunc.apply(bmw, [ 'Бампер', 2]);
-addDamageFunc.apply(audi, [ 'Бампер', 2]);
+const addDamageAudRoof = carManipulation.addDamage.bind(audi, 'Крыша');
+addDamageAudRoof(2);
+console.log(audi);
