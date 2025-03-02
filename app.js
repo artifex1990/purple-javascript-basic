c9a666356718c45cbfa485c081eecfa957f4615f
pavel demyanov <pavel@demjanov.ru>
2025-03-02 10:09:36 +0300

    Call, apply

--- app.js
+++ app.js
@@ -1,15 +1,34 @@
 'use strict';
 
-const b = 1;
-
-const a = {
-    b,
-    getB: function () {
-        return this.b;
-    },
-    getBAlt() {
-        return this.b;
+const audi = {
+    make: 'Audi',
+    model: 'A4',
+    year: 2020,
+    damages: [],
+    addDamage(part, rate) {
+        console.log(
+            `У авто ${this.make} ${this.model} ${this.year} добавлено повреждение ${part} со степенью ${rate}.`
+        );
+        this.damages.push({part, rate});
     }
 }
+//audi.addDamage('капот', 1);
 
-console.log(a.getBAlt());
+const bmw = {
+    make: 'BMW',
+    model: 'X5',
+    year: 2022,
+    damages: [],
+};
+
+bmw.addDamage = audi.addDamage;
+//bmw.addDamage('Бампер', 2);
+
+
+const addDamageFunc = audi.addDamage;
+//addDamageFunc('Бампер', 2);
+addDamageFunc.call(bmw, ...['Бампер', 2]);
+addDamageFunc.call(audi, 'Бампер', 2);
+
+addDamageFunc.apply(bmw, [ 'Бампер', 2]);
+addDamageFunc.apply(audi, [ 'Бампер', 2]);
