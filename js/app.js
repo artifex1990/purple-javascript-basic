58529ce4f47f2f2dcca264ed843225f9242726b2
pavel demyanov <pavel@demjanov.ru>
2025-03-03 23:24:00 +0300

    Загрузка и сохранение данных

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
 'use strict';
 
+let habbits = [];
+const HABBIT_KEY = 'HABBIT_KEY';
 
+function loadData() {
+    const habbitsString = localStorage.getItem(HABBIT_KEY);
+    const habbitArray = JSON.parse(habbitsString);
+    if (Array.isArray(habbitArray)) {
+        habbits = habbitArray;
+    }
+}
 
+function saveData() {
+    localStorage.setItem(HABBIT_KEY, JSON.stringify(habbits));
+}
 
+(() => {
+    loadData();
+})();
