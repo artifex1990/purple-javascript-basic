6c5dd99a5d3026c0a52f651005ff88eec98a4d34
pavel demyanov <pavel@demjanov.ru>
2025-03-04 13:55:28 +0300

    Добавление дня

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@
 
 let habbits = [];
 const HABBIT_KEY = 'HABBIT_KEY';
+let globalActiveHabbitId;
 
 /* page */
 const page = {
@@ -84,6 +85,7 @@ function rerenderContent(activeHabbit) {
 }
 
 function rerender(activeHabbitId) {
+    globalActiveHabbitId = +activeHabbitId;
     const activeHabbit = habbits.find(habbit => habbit.id === activeHabbitId);
 
     if (!activeHabbit) {
@@ -99,7 +101,28 @@ function addDays(event) {
     event.preventDefault();
 
     const data = new FormData(event.target);
-    console.log(data.get('comment'));
+    const form = event.target;
+    const comment = data.get('comment');
+
+    form['comment'].classList.remove('error');
+    if (!comment) {
+        form['comment'].classList.add('error');
+    }
+
+    habbits = habbits.map(habbit => {
+        if (habbit.id === globalActiveHabbitId) {
+            return {
+                ...habbit,
+                days: habbit.days.concat([{ comment }])
+            }
+        }
+
+        return habbit;
+    });
+
+    form['comment'].value = '';
+    rerender(globalActiveHabbitId);
+    saveData();
 } 
 
 /* init */
